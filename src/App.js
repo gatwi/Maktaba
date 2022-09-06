2be91c839d18f9c0e7a2880172cfd0a742b86f74
gatwi <faith.murithi@student.moringaschool.com>
2022-09-06 14:30:20 +0300

    Additional management on the Nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Home from './pages/Home';
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 
 export const App = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
           <MenuItem>
-            <Badge badgeContent={4} color="primary">
+            <Badge badgeContent={2} color="primary">
               <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
