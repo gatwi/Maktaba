d44be57a0fc50f7b066c02607b729c1357d62cc3
gatwi <faith.murithi@student.moringaschool.com>
2022-09-08 00:30:48 +0300

    Removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Home from './pages/Home';
-import Register from './pages/Register';
+import Catalog from "./pages/Catalog";
+import Library from "./pages/Library";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 
 
-export const App = () => {
-  return <Home/>
+const App = () => {
+  return (
+    <Home />
+  );
 };
 
 export default App;
